572c8265b565d7be1eac385dd3c4f078567b2943
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2023-07-12 18:51:23 -0400

    solved 543

--- leetcode/543-diameterOfBinaryTree.js
+++ leetcode/543-diameterOfBinaryTree.js
@@ -1,7 +1,17 @@
-const assert = require("assert");
+const assert = require("node:assert");
+const { describe, test } = require("node:test");
 const { createTree } = require("../lib");
 
 // https://leetcode.com/problems/diameter-of-binary-tree/
+
+/*
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+*/
 
 /**
  * find longest path of connected edges.  2 connected nodes is 1 edge
@@ -10,62 +20,41 @@ const { createTree } = require("../lib");
  * @param {TreeNode} root
  * @return {number}
  */
-var diameterOfBinaryTree = function (root) {
-  if (root === null) return 0;
-  if (root.left === null && root.right === null) return 1;
+const diameterOfBinaryTree = function (root) {
+  let max = 0;
 
-  preOrder(root);
-
-  let leftEdges = 0;
-  let rightEdges = 0;
-  const parents = [root];
-
-  while (parents.length) {
-    const [node] = parents.splice(0, 1);
-    if (node.left) {
-      leftEdges += 1;
-    }
-    if (node.left?.left || node.left?.right) {
-      parents.push(node.left);
-    }
-    if (node.right) {
-      rightEdges += 1;
-    }
-    if (node.righ?.left || node.right?.right) {
-      parents.push(node.right);
-    }
+  function dfs(r) {
+    if (!r) return -1;
+    const left = dfs(r.left);
+    const right = dfs(r.right);
+    max = Math.max(max, 2 + left + right);
+    return 1 + Math.max(left, right);
   }
-  console.log(leftEdges, rightEdges);
+
+  dfs(root);
+  return max;
 };
 
-function preOrder(root) {
-  if (root) {
-    console.log(root.val);
-    preOrder(root.left);
-    preOrder(root.right);
-  }
-}
-
 describe("diameterOfBinaryTree", function () {
-  it("Test Case 1", function () {
+  test("Test Case 1", function () {
     const actual = diameterOfBinaryTree(createTree([1, 2, 3, 4, 5]));
     const expected = 3;
     assert.strictEqual(actual, expected);
   });
 
-  it("Test Case 2", function () {
+  test("Test Case 2", function () {
     const actual = diameterOfBinaryTree(createTree([1, 2]));
     const expected = 1;
     assert.strictEqual(actual, expected);
   });
 
-  it("Test Case 3", () => {
+  test("Test Case 3", () => {
     const actual = diameterOfBinaryTree(createTree([3, 1, null, null, 2]));
     const expected = 2;
     assert.strictEqual(actual, expected);
   });
 
-  it("Test Case 4", () => {
+  test("Test Case 4", () => {
     const actual = diameterOfBinaryTree(createTree([1, null, 2]));
     const expected = 1;
     assert.strictEqual(actual, expected);
