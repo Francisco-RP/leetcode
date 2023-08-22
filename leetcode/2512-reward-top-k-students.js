8541c8a37dcca0bb83b0174f2e992a7ad862a786
Francisco-RP <80427096+Francisco-RP@users.noreply.github.com>
2023-08-22 11:57:25 -0400

    updated 2512

--- leetcode/2512-reward-top-k-students.js
+++ leetcode/2512-reward-top-k-students.js
@@ -30,16 +30,14 @@ const topStudents = function (positive_feedback, negative_feedback, report, stud
     map.set(id, getScore(report[i]));
   });
 
-  const sortedMap = new Map(
-    [...map.entries()].sort((a, b) => {
-      if (b[1] === a[1]) {
-        return a[0] - b[0];
-      }
-      return b[1] - a[1];
-    })
-  );
+  const sortedMap = [...map.entries()].sort((a, b) => {
+    if (b[1] === a[1]) {
+      return a[0] - b[0];
+    }
+    return b[1] - a[1];
+  });
 
-  return [...sortedMap.keys()].slice(0, k);
+  return sortedMap.slice(0, k).map((keyVal) => keyVal[0]);
 };
 
 describe("topStudents", function () {
